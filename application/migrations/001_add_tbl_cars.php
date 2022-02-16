d26f912e6043bdaa93cbff0c0df8b968c8bb3454
Zoltán Szabó <szabozli@gmail.com>
2022-02-16 14:38:00 +0100

    Cars database table create with migration

---
+++ application/migrations/001_add_tbl_cars.php
@@ -0,0 +1,38 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_Tbl_Cars extends CI_Migration {
+
+	public function up()
+	{
+		$this->dbforge->add_field(array(
+			'id' => array(
+				'type' => 'INT',
+				'constraint' => 10,
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE,
+				'null' => FALSE,
+			),
+			'license_plate' => array(
+				'type' => 'VARCHAR',
+				'constraint' => 7,
+				'unique' => TRUE,
+				'null' => FALSE,
+			),
+			'registrarion_year' => array(
+				'type' => 'INT',
+				'null' => TRUE,
+				'constraint' => 4,
+				'null' => FALSE,
+			),
+		));
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table('cars');
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_table('cars');
+	}
+}
